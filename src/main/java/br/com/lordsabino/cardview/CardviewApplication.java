1dd93aee0d1a3617aa81af80f7dbd117b163b0c2
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2026-02-10 15:53:34 -0300

    projeto criado

---
+++ src/main/java/br/com/lordsabino/cardview/CardviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.lordsabino.cardview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lordsabino/cardview/CardviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lordsabino.cardview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
